e2875af57f864d2e22b6caed6cf106930ffdd890
sonali <kamblesonali185@gmail.com>
2022-05-17 11:08:13 +0530

    added pipe, directive and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HighlightDirective } from './highlight.directive';
+import { ImgurlPipe } from './imgurl.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent,
+    UserComponent,
+    UserListComponent,
+    HighlightDirective,
+    ImgurlPipe
   ],
   imports: [
     BrowserModule
